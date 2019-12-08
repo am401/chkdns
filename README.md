f556cc79c7cd666b658d72db359672f061b57083
am401 <rx22ee@gmail.com>
2019-12-08 11:46:46 -0600

    Initial script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # chkdns
-Check IP where domiain is pointed
+
+# Information #
+
+Script to display the IP address domain names are resolving to. The script accepts the domain name as a command line argument. It strips away the protocol and any trailing paths or details.
+
+If the IP is utilizing CloudFlare - the script will also identify this.
+
+# Usage #
+
+./chkdns.sh example.com
---
+++ chkdns.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#  Script to check for just the IP address of a domain
+#  by Andras Marton
+#  date: 8th December 2019
+
+#  Grab domain name from cmd line argument
+SITE=$1
+
+function usage() {
+
+cat <<- EOF
+	Usage for $0:
+	A valid domain name must be supplied to use this script:
+	$0 example.com
+EOF
+}
+
+function cloudflare() {
+	
+	curl -ILs $SITE | grep -i "cloudflare" &>/dev/null
+	
+		local result=$?
+
+	if [[ $result -eq 0 ]]
+	then
+		printf "Domain is going through CloudFlare\n"
+	fi
+}
+
+function main() {
+	#  Remove protocol and trailing path info from domain
+	if [[ "$SITE" == http://* ]] || [[ "$SITE" == https://* ]]
+	then
+		SITE=`echo $SITE | awk -F/ '{print $3}'`
+		dig $SITE +short
+		cloudflare
+	else
+		dig $SITE +short
+		cloudflare
+	fi
+}
+
+#  Check if an argument was passed to script
+if [[ -z $1 ]]
+then
+	usage
+	exit 1
+fi
+
+#  Check string to make sure it does not contain illegal characters
+if [[ "$SITE" =~ ^[a-zA-Z0-9\.\-:/]+$ ]]
+then
+	#  Run the main script
+	main
+else
+	usage
+	exit 1
+fi
